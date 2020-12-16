2cce0aca0aff4eb96ab477b86477b1b618c9d51c
Clemence Kyara <clemence@codeforafrica.org>
2020-12-16 21:57:31 +0300

    Add ability to save progress back to gSheet

--- chalicelib/sheets.py
+++ chalicelib/sheets.py
@@ -1,6 +1,17 @@
+import arrow
 import gspread
 
 from chalicelib import settings
+
+# Must be ordered as they appear in gSheet
+SHEET_COLUMNS = [
+    "page_size",
+    "start_date",
+    "count",
+    "last_end_date",
+    "last_count",
+]
+SHEET_RANGE = "A2:E2"
 
 
 class ApiParametersSheet:
@@ -14,25 +25,32 @@ class ApiParametersSheet:
             return self._parameters
 
         parameters = {}
+        record = {}
         records = self._worksheet.get_all_records()
         if len(records):
             # We're just interested in the first row of data
             record = records[0]
-            limit = record.get(settings.SHEETS_SHEET_COLUMN_PAGE_SIZE)
-            parameters["limit"] = (
-                limit if limit else settings.OPENAQ_API_DEFAULT_PAGE_SIZE
-            )
-            # OpenAQ allows data access via API only up to 2018-01-01
-            # see:https://openaq.medium.com/openaq-extends-api-and-download-tool-access-from-90-days-to-2-years-3697540c85a3
-            date_from = record.get(settings.SHEETS_SHEET_COLUMN_START_DATE)
-            parameters["date_from"] = (
-                date_from if date_from and date_from.strip() else "2018-01-01"
-            )
-            last_end_date = record.get(settings.SHEETS_SHEET_COLUMN_LAST_END_DATE)
-            if last_end_date:
-                parameters["date_from"] = last_end_date
-            count = record.get(settings.SHEETS_SHEET_COLUMN_COUNT)
-            parameters["count"] = count if count else 0
-            self._parameters = parameters
+        page_size = record.get(settings.SHEETS_SHEET_COLUMN_PAGE_SIZE)
+        parameters["page_size"] = (
+            page_size if page_size else settings.OPENAQ_API_DEFAULT_PAGE_SIZE
+        )
+        # OpenAQ allows data access via API only up to 2018-01-01
+        # see:https://openaq.medium.com/openaq-extends-api-and-download-tool-access-from-90-days-to-2-years-3697540c85a3
+        start_date = record.get(settings.SHEETS_SHEET_COLUMN_START_DATE)
+        if start_date and start_date.strip():
+            parameters["start_date"] = arrow.get(start_date.strip()).isoformat()
+        count = record.get(settings.SHEETS_SHEET_COLUMN_COUNT)
+        parameters["count"] = count if count else 0
+        last_end_date = record.get(settings.SHEETS_SHEET_COLUMN_LAST_END_DATE)
+        if last_end_date and last_end_date.strip():
+            parameters["last_end_date"] = last_end_date.strip()
+        last_count = record.get(settings.SHEETS_SHEET_COLUMN_LAST_COUNT)
+        parameters["last_count"] = last_count if last_count else 0
+        self._parameters = parameters
 
         return self._parameters
+
+    def save(self, parameters):
+        updated_parameters = {**self._parameters, **parameters}
+        updated_values = list(map(lambda x: updated_parameters[x], SHEET_COLUMNS))
+        self._worksheet.update(SHEET_RANGE, [updated_values])
